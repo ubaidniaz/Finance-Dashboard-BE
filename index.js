8c5dc6bc9acf23da094a5648ba17ff5ce31fb770
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 14:29:50 -0700

    install/figure out mongoose/ MongoDB and running on correct port

--- index.js
+++ index.js
@@ -6,17 +6,23 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 
-
 /* CONFIGURATIONS */
-dotenv.config()
+dotenv.config();
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(cors())
-
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
