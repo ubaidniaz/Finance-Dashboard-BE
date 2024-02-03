edafd2f8a5c3023ebee7fdaf5e6718de25474f7e
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 15:08:39 -0700

    setup totalProfit KpiSchema

--- models/KPI.js
+++ models/KPI.js
@@ -3,10 +3,16 @@ import { loadType } from "mongoose-currency";
 
 
 const Schema = mongoose.Schema;
+loadType(mongoose);
 
 const KPISchema = new Schema(
-
-
+    {
+        totalProfit: {
+            type: mongoose.Types.Currency,
+            currency: "USD", 
+            get: (v) => v / 100
+        }
+    }
 );
 
 const KPI = mongoose.model("KPI", KPISchema);
