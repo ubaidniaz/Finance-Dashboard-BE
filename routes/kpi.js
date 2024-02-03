5f4cfae3ea3626d267ff3cc12a37fb84225b735a
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-03 16:00:53 -0700

    fix import causing eroor

--- routes/kpi.js
+++ routes/kpi.js
@@ -1,14 +1,14 @@
 import express from "express";
-import KPI from "../models/KPI.js";
+import KPI from "../models/KPI.js"; // This is your model import
 
 const router = express.Router();
 
 router.get("/kpis", async (req, res) => {
-    try{
-        const kpis = await kpis.find();
+    try {
+        const kpis = await KPI.find(); // Use 'KPI' to refer to your model
         res.status(200).json(kpis);
-    } catch(error) {
-        res.status(404).json({ message: error.message});
+    } catch (error) {
+        res.status(404).json({ message: error.message });
     }
 });
 
