26ae2ec8019ccd6256c907ba366d87ff2c99191a
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-04 22:09:09 -0700

    insert product data into mongodb

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js"
 import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import { kpis, products } from "./data/data.js";
 
 
 /* CONFIGURATIONS */
@@ -34,8 +34,9 @@ mongoose
   .then(async() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
    /* ADD DATA ONE TIMEONLY OR AS NEEDED */
-   //  await mongoose.connection.db.dropDatabase();
-   //  KPI.insertMany(kpis);
+   // await mongoose.connection.db.dropDatabase();
+   // KPI.insertMany(kpis);
+   // Product.insertMany(products);
   })
   .catch((error) => console.log(`${error} did not connect`));
 
