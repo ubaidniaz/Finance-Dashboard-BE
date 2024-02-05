9e702e5ef6462be1eb1d1a9b3bcc7bc00b7ece05
wisepakiguy <ubaidullahahsan@gmail.com>
2024-02-05 00:25:34 -0700

    create transaction route

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
 import productRoutes from "./routes/product.js"
+import transactionRoutes from "./routes/transaction.js"
 import Product from "./models/Product.js";
+import Transaction from "./models/Transaction.js"
 import KPI from "./models/KPI.js";
-import { kpis, products } from "./data/data.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 
 /* CONFIGURATIONS */
@@ -26,6 +28,7 @@ app.use(cors());
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
 app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
